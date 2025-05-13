7837231905a00b93bbd54c57d9b2b979c8342a61
Jungu Lee <prnsslcn@gmail.com>
2025-05-14 00:39:14 +0900

    remove: remove all
    The topic is scheduled to change

--- backend/src/main/java/com/jobfair/booth/repository/BoothRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jobfair.booth.repository;
-
-import com.jobfair.booth.model.Booth;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoothRepository extends JpaRepository<Booth, Long> {
-}
